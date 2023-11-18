45570dab4d30b1088784b45aa9ba6f3c0771bce8
Ahmed <ahmedthecat3457@gmail.com>
2023-11-19 00:25:25 +0300

    hh

---
+++ main.py
@@ -0,0 +1,17 @@
+import nextcord
+from nextcord.ext import commands
+
+
+bot = commands.Bot()
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.slash_command(description="Send a message to someone", force_global = True, dm_permission = True)
+async def send(interaction: nextcord.Interaction, ide: int = nextcord.SlashOption(required=True), message: str = nextcord.SlashOption(required=True)):
+    await interaction.response.defer()
+    mr = bot.get_user(ide)
+    mr.send(message)
+
+bot.run("hellow")
