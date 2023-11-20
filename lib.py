60334c72627f2af9a2424ae3d7bbcdc9d9a25292
Ahmed <ahmedthecat3457@gmail.com>
2023-11-20 18:24:37 +0300

    improvements

--- lib.py
+++ lib.py
@@ -1,58 +1,64 @@
 from random import shuffle, choice
 cubes = {
-        "222": [
+        "222": [[
             "R", "R'", "R2",
             "L", "L'", "L2",
             "U", "U'", "U2",
             "D", "D'", "D2",
             "F", "F'", "F2",
             "B", "B'", "B2"
-        ],
-        "333": [
+        ], "2x2"],
+        "333": [[
             "R", "R'", "R2",
             "L", "L'", "L2",
             "U", "U'", "U2",
             "D", "D'", "D2",
             "F", "F'", "F2",
             "B", "B'", "B2"
-        ],
-        "444": [
+        ], "3x3"],
+        "444": [[
             "R", "R'", "R2", "Rw", "Rw'", "Rw2",
             "L", "L'", "L2", "Lw", "Lw'", "Lw2",
             "U", "U'", "U2", "Uw", "Uw'", "Uw2",
             "D", "D'", "D2", "Dw", "Dw'", "Dw2",
             "F", "F'", "F2", "Fw", "Fw'", "Fw2",
             "B", "B'", "B2", "Bw", "Bw'", "Bw2"
-        ],
-        "555": [
+        ], "4x4"],
+        "555": [[
             "R", "R'", "R2", "Rw", "Rw'", "Rw2",
             "L", "L'", "L2", "Lw", "Lw'", "Lw2",
             "U", "U'", "U2", "Uw", "Uw'", "Uw2",
             "D", "D'", "D2", "Dw", "Dw'", "Dw2",
             "F", "F'", "F2", "Fw", "Fw'", "Fw2",
             "B", "B'", "B2", "Bw", "Bw'", "Bw2"
-        ],
-        "666": [
+        ], "5x5"],
+        "666": [[
             "R", "R'", "R2", "Rw", "Rw'", "Rw2",
             "L", "L'", "L2", "Lw", "Lw'", "Lw2",
             "U", "U'", "U2", "Uw", "Uw'", "Uw2",
             "D", "D'", "D2", "Dw", "Dw'", "Dw2",
             "F", "F'", "F2", "Fw", "Fw'", "Fw2",
             "B", "B'", "B2", "Bw", "Bw'", "Bw2"
-        ],
-        "777": [
+        ], "6x6"],
+        "777": [[
             "R", "R'", "R2", "Rw", "Rw'", "Rw2",
             "L", "L'", "L2", "Lw", "Lw'", "Lw2",
             "U", "U'", "U2", "Uw", "Uw'", "Uw2",
             "D", "D'", "D2", "Dw", "Dw'", "Dw2",
             "F", "F'", "F2", "Fw", "Fw'", "Fw2",
             "B", "B'", "B2", "Bw", "Bw'", "Bw2"
-        ]
+        ], "7x7"]
 }
 def gen_scramble(length=20, cube="333"):
     scramble=""
     for i in range(length):
-        shuffle(cubes[cube])
-        x = choice(cubes[cube])
+        shuffle(cubes[cube][0])
+        x = choice(cubes[cube][0])
         scramble += x + " "
-    return scramble
+    return (scramble, cubes[cube][1])
+
+def get_all_cubes():
+    names = []
+    for cube in cubes:
+        names.append(cubes[cube][1])
+    return names
--- main.py
+++ main.py
@@ -13,14 +13,17 @@ test_servers = [937639594030153758]
 async def on_ready():
     print(f"We have logged in as {bot.user}")
 
-@bot.slash_command(description = "Scramble a cube", force_global = True, dm_permission = True, guild_ids=test_servers)
-async def scramble(interaction: nextcord.Interaction, length: int = nextcord.SlashOption(description = "The length of the scramble.", required = False), cube: str = nextcord.SlashOption(description = "The cube you want to scramble.", required = False, choices = cubes.keys())):
+@bot.slash_command(description = "Generate a scramble for the chosen cube.", force_global = True, dm_permission = True, guild_ids=test_servers)
+async def scramble(interaction: nextcord.Interaction, length: int = nextcord.SlashOption(description = "The length of the scramble, 50 at maximum.", required = False), cube: str = nextcord.SlashOption(description = "The cube you want to scramble.", required = False, choices = cubes.keys())):
+    user = interaction.user
     await interaction.response.defer()
-    if length > 50 or length == None or length < 1:
+    if length == None or length > 50 or length < 1:
         length = 20
     if cube == None:
         cube = "333"
     scramble = gen_scramble(length=length, cube=cube)
-    await interaction.followup.send(f"Here is your scramble <@{interaction.user.id}>: ```{scramble}```")
+    embed = nextcord.Embed(title=f"{scramble[1]} Scramble", description=f"Here's your scramble:\n`{scramble[0]}`")
+    embed.set_footer(text=f"<@{user.id}>", icon_url=user.avatar.url)
+    await interaction.followup.send(embed=embed)
     
 bot.run(token)
